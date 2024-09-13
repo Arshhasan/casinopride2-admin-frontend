7aaa5ed8a95a9b174a442fc4e3596327d7dbb2c3
abhinav0877 <technoxpert12@gmail.com>
2024-09-13 11:37:52 +0530

    Changed `http` to `https`

--- src/Layouts/Pages/Billing/TeensBilling.js
+++ src/Layouts/Pages/Billing/TeensBilling.js
@@ -934,7 +934,7 @@ const TeensBilling = () => {
                           })
                         );
 
-                        const apiUrl = `http://commnestsms.com/api/push.json?apikey=635cd8e64fddd&route=transactional&sender=CPGOAA&mobileno=${BookingDetails[0]?.Phone}&text=Thank%20you%20for%20choosing%20Casino%20Pride.%20View%20e-bill%20of%20Rs%20${FinalAmount}%20at%20-%20${callback?.response?.shortUrl}%0ALets%20Play%20with%20Pride%20!%0AGood%20luck%20!%0ACPGOAA`;
+                        const apiUrl = `https://commnestsms.com/api/push.json?apikey=635cd8e64fddd&route=transactional&sender=CPGOAA&mobileno=${BookingDetails[0]?.Phone}&text=Thank%20you%20for%20choosing%20Casino%20Pride.%20View%20e-bill%20of%20Rs%20${FinalAmount}%20at%20-%20${callback?.response?.shortUrl}%0ALets%20Play%20with%20Pride%20!%0AGood%20luck%20!%0ACPGOAA`;
                         fetch(apiUrl)
                           .then((response) => {
                             if (!response.ok) {
